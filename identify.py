0c759e8cf40213b8582807711586d1929d992882
ross <2086516395@qq.com>
2023-01-06 07:02:02 +0800

    fourth commit

--- identify.py
+++ identify.py
@@ -261,5 +261,136 @@ class CardPredictor:
         return xl, xr, yh, yl
 
 
+    def predict(self, car_pic):
+        if type(car_pic) == type(""):
+            img = imreadex(car_pic)
+        else:
+            img = car_pic
+        pic_hight, pic_width = img.shape[:2]
+
+        if pic_width > MAX_WIDTH:
+            resize_rate = MAX_WIDTH / pic_width
+            img = cv2.resize(img, (MAX_WIDTH, int(pic_hight * resize_rate)), interpolation=cv2.INTER_AREA)
+
+        blur = self.cfg["blur"]
+        # 高斯去噪
+        if blur > 0:
+            img = cv2.GaussianBlur(img, (blur, blur), 0)  # 图片分辨率调整
+        oldimg = img
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        # equ = cv2.equalizeHist(img)
+        # img = np.hstack((img, equ))
+        # 去掉图像中不会是车牌的区域
+        kernel = np.ones((20, 20), np.uint8)
+        img_opening = cv2.morphologyEx(img, cv2.MORPH_OPEN, kernel)
+        img_opening = cv2.addWeighted(img, 1, img_opening, -1, 0);
+
+        # 找到图像边缘
+        ret, img_thresh = cv2.threshold(img_opening, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+        img_edge = cv2.Canny(img_thresh, 100, 200)
+        # 使用开运算和闭运算让图像边缘成为一个整体
+        kernel = np.ones((self.cfg["morphologyr"], self.cfg["morphologyc"]), np.uint8)
+        img_edge1 = cv2.morphologyEx(img_edge, cv2.MORPH_CLOSE, kernel)
+        img_edge2 = cv2.morphologyEx(img_edge1, cv2.MORPH_OPEN, kernel)
+        # 查找图像边缘整体形成的矩形区域，可能有很多，车牌就在其中一个矩形区域中
+        try:
+            contours, hierarchy = cv2.findContours(img_edge2, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+        except ValueError:
+            image, contours, hierarchy = cv2.findContours(img_edge2, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+        contours = [cnt for cnt in contours if cv2.contourArea(cnt) > Min_Area]
+        print('len(contours)', len(contours))
+        # 一一排除不是车牌的矩形区域
+        car_contours = []
+        for cnt in contours:
+            rect = cv2.minAreaRect(cnt)
+            area_width, area_height = rect[1]
+            if area_width < area_height:
+                area_width, area_height = area_height, area_width
+            wh_ratio = area_width / area_height
+            # print(wh_ratio)
+            # 要求矩形区域长宽比在2到5.5之间，2到5.5是车牌的长宽比，其余的矩形排除
+            if wh_ratio > 2 and wh_ratio < 5.5:
+                car_contours.append(rect)
+                box = cv2.boxPoints(rect)
+                box = np.int0(box)
+        print(len(car_contours))
+        print("精确定位")
+        card_imgs = []
+        # 矩形区域可能是倾斜的矩形，需要矫正，以便使用颜色定位
+        for rect in car_contours:
+            if rect[2] > -1 and rect[2] < 1:  # 创造角度，使得左、高、右、低拿到正确的值
+                angle = 1
+            else:
+                angle = rect[2]
+            rect = (rect[0], (rect[1][0] + 5, rect[1][1] + 5), angle)  # 扩大范围，避免车牌边缘被排除
+
+            box = cv2.boxPoints(rect)
+            heigth_point = right_point = [0, 0]
+            left_point = low_point = [pic_width, pic_hight]
+            for point in box:
+                if left_point[0] > point[0]:
+                    left_point = point
+                if low_point[1] > point[1]:
+                    low_point = point
+                if heigth_point[1] < point[1]:
+                    heigth_point = point
+                if right_point[0] < point[0]:
+                    right_point = point
+
+            if left_point[1] <= right_point[1]:  # 正角度
+                new_right_point = [right_point[0], heigth_point[1]]
+                pts2 = np.float32([left_point, heigth_point, new_right_point])  # 字符只是高度需要改变
+                pts1 = np.float32([left_point, heigth_point, right_point])
+                M = cv2.getAffineTransform(pts1, pts2)
+                dst = cv2.warpAffine(oldimg, M, (pic_width, pic_hight))
+                point_limit(new_right_point)
+                point_limit(heigth_point)
+                point_limit(left_point)
+                card_img = dst[int(left_point[1]):int(heigth_point[1]), int(left_point[0]):int(new_right_point[0])]
+                card_imgs.append(card_img)
+            # cv2.imshow("card", card_img)
+            # cv2.waitKey(0)
+            elif left_point[1] > right_point[1]:  # 负角度
+
+                new_left_point = [left_point[0], heigth_point[1]]
+                pts2 = np.float32([new_left_point, heigth_point, right_point])  # 字符只是高度需要改变
+                pts1 = np.float32([left_point, heigth_point, right_point])
+                M = cv2.getAffineTransform(pts1, pts2)
+                dst = cv2.warpAffine(oldimg, M, (pic_width, pic_hight))
+                point_limit(right_point)
+                point_limit(heigth_point)
+                point_limit(new_left_point)
+                card_img = dst[int(right_point[1]):int(heigth_point[1]), int(new_left_point[0]):int(right_point[0])]
+                card_imgs.append(card_img)
+            # cv2.imshow("card", card_img)
+            # cv2.waitKey(0)
+        # 开始使用颜色定位，排除不是车牌的矩形，目前只识别蓝、绿、黄车牌
+        colors = []
+        for card_index, card_img in enumerate(card_imgs):
+            green = yello = blue = black = white = 0
+            card_img_hsv = cv2.cvtColor(card_img, cv2.COLOR_BGR2HSV)
+            # 有转换失败的可能，原因来自于上面矫正矩形出错
+            if card_img_hsv is None:
+                continue
+            row_num, col_num = card_img_hsv.shape[:2]
+            card_img_count = row_num * col_num
+
+            for i in range(row_num):
+                for j in range(col_num):
+                    H = card_img_hsv.item(i, j, 0)
+                    S = card_img_hsv.item(i, j, 1)
+                    V = card_img_hsv.item(i, j, 2)
+                    if 11 < H <= 34 and S > 34:  # 图片分辨率调整
+                        yello += 1
+                    elif 35 < H <= 99 and S > 34:  # 图片分辨率调整
+                        green += 1
+                    elif 99 < H <= 124 and S > 34:  # 图片分辨率调整
+                        blue += 1
+
+                    if 0 < H < 180 and 0 < S < 255 and 0 < V < 46:
+                        black += 1
+                    elif 0 < H < 180 and 0 < S < 43 and 221 < V < 225:
+                        white += 1
+
 
 
